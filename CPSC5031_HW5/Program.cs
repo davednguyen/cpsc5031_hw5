9e27c8d529ebf1aee9e3cb8a1ea47f14ee38c715
dnguyen <david.nguyen@smartsheet.com>
2021-05-11 16:20:51 -0700

    first commit for  the project.

---
+++ CPSC5031_HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPSC5031_HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
